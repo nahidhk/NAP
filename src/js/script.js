521723460641e1bd453e076f29f364a0f58e5e45
nahidhk <nahidappltd@gmail.com>
2025-05-23 16:42:39 +0600

    new update

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-function showpass(){
+function showpass() {
     var passwordinputTag = document.getElementsByClassName("password")[0];
     var passiconTag = document.getElementsByClassName("passicon")[0];
     if (passwordinputTag.type === "password") {
@@ -10,4 +10,27 @@ function showpass(){
         passiconTag.classList.remove("fa-eye-slash");
         passiconTag.classList.add("fa-eye");
     }
+}
+
+const username = document.getElementById("username");
+const key = document.getElementById("key");
+
+
+
+
+
+
+
+
+function login() {
+    const loginform = document.getElementById("loginform");
+    const mypassword = document.getElementById("password").value;
+
+    if (username.value === dBusername && password === dBpassword && key.value === dBtoken) {
+        alert("Login successful");
+        // Redirect to another page or perform any other action
+    } else {
+        alert("Login failed");
+    }
+
 }

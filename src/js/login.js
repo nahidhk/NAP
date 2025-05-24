9b33d2f1e86bb0e22fd0c7b941633b14ca698854
nahidhk <nahidappltd@gmail.com>
2025-05-24 21:58:14 +0600

    new update

--- src/js/login.js
+++ src/js/login.js
@@ -1,8 +1,9 @@
 // Install python and config JSON !
-
+console.log("Login script loaded");
 const youlogin = sessionStorage.getItem("login");
-if (login === "true") {
-  
+if (youlogin === "true") {
+
 } else {
-  
+alert("You are not logged in! Please log in to continue.");
+window.location.href = "/";
 }
---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+console.log("Main script loaded");
+function logouti(){
+    sessionStorage.clear();
+    window.location.reload();
+}
